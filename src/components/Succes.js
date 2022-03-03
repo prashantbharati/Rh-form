517825cc3ca76c183ebb2134a6487bcf854e6f8f
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-03-03 22:38:58 +0530

    result added sucess

--- src/components/Succes.js
+++ src/components/Succes.js
@@ -4,8 +4,24 @@ import { Typography } from "@material-ui/core";
 const App = (props) => {
   const classes = useStyles();
 
+  // Area of the rooftop - i/p sq ft *0.092
+  // Select city - Store data from the website - i/p city -o/p rainfall in mm
+  // Total rain water
+  // = Rainfall (mm) x Area of Rooftop x 0.9 *1000
+
   const user = props.user,
-    setuser = props.setuser;
+    setuser = props.setuser,
+    city = props.city;
+
+  const result = () => {
+    let Area = user * 0.092;
+    let rainfall = 100;
+    const answer = rainfall * Area * 0.9 * 100;
+    return answer;
+  };
+
+  console.log(result);
+
   console.log(user);
   const lol = "hello";
   return (
