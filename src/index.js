213952f224a50fa1023c44f0b12a043ea614c09c
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-03-04 22:06:14 +0530

    map rendering

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 
-import App from "./App";
+// import App from "./App";
+import App2 from "./components/App2.js";
 import theme from "./theme";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <App />
+    <App2 />
   </ThemeProvider>,
   document.getElementById("root")
 );
