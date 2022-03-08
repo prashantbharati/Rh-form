fd3c0352a1102386bd20b03aba08287e57bf9fde
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-03-08 19:21:26 +0530

    removed all html add files

--- linking/index.js
+++
@@ -1,4 +0,0 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
-
-ReactDOM.render(<div>Hello React</div>, document.getElementById("app"));
--- newindex.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const App = () => {
-  return <div>HEy sup;</div>;
-};
-export default App;
